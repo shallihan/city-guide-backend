2a123122e33847489c014b91970fdfd608fdc583
shallihan <smhallihan@gmail.com>
2021-06-21 11:19:37 +0100

    Connect DELETE place request to MongoDB.

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -123,12 +123,23 @@ const updatePlace = async (req, res, next) => {
   res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
-  if (!PLACES.find((place) => place.id === placeId)) {
-    throw new HttpError("Could not find a place to delete", 404);
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not delete place', 500);
+    return next(error);
   }
-  PLACES = PLACES.filter((place) => place.id !== placeId);
+
+  try {
+    await place.remove();
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not delete place', 500);
+    return next(error);
+  }
+  
   res.status(200).json({ message: "Deleted" });
 };
 
